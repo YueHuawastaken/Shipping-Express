5fd6d547a035f397561346ace47c3a4fc3db24d9
YueHuawastaken Hua <yu3hua@gmail.com>
2024-03-14 07:14:43 +0000

    final commit

--- 01-Shipping/index.js
+++ 01-Shipping/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require("cors");
 const mongodb = require ('mongodb');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 require ('dotenv').config();
 
 const MongoClient = mongodb.MongoClient;
@@ -41,9 +43,49 @@ async function main() {
   }
 }
 
+function authenticateWithJWT(req, res, next) {
+  
+  // 2nd mistake is, req.headers.authorization does not magically appear out from fantasy land
+  // req.headers.authorization = JWTStore["access_token"];
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  console.log("authheader here", authHeader)
+  if (authHeader) {
+
+      // 3rd mistake: authHeader.split(" ")[1];
+      // JWT traditionally comes in the form of:
+      // BEARER "actual hash of the token is here with a space after bearer"
+      // .split takes each element separated by a empty space " " and put into an array and returns it
+      // and thus ["BEARER", "JSONHASHBLAHBLAH"]
+      // thus token = theArray[1] which is the 2nd position which is actual token
+
+      const token = authHeader.split(" ")[1];
+      // first argument: the token that I want to verify
+      // second argument: the token secret
+      // third argument: callback function
+      jwt.verify(token, process.env.TOKEN_SECRET, function(err,payload){
+          if (err) {
+              res.status(400);
+              return res.json({
+                  'error': err
+              })
+          } else {
+              // the JWT is valid, forward request to the route and store the payload in the request
+              req.payload = payload;
+              next();
+          }
+      })
+  } else {
+      res.status(400);
+      res.json({
+          'error':'Login required to access this route'
+      })
+  }
+}
+
 main();
  
-app.post('/shippingproducts', async(req,res) => {
+app.post('/shippingproducts',authenticateWithJWT, async(req,res) => {
   try {
 
       console.log("route hit")
@@ -68,15 +110,15 @@ app.post('/shippingproducts', async(req,res) => {
   }
 })
 
-app.get('/shippingproducts', async (req, res) => {
+app.get('/shippingproducts',authenticateWithJWT, async (req, res) => {
   try {
     const allShippingItems = await db.collection('shipping_db').find({}).toArray();
-    console.log("Shipping Items in all fetch", allShippingItems)
+    // console.log("Shipping Items in all fetch", allShippingItems)
     // res.json(allShippingItems);
 
     const tags = await db.collection('product_Type').find({}).toArray();
     
-    console.log ("all tags here", tags)
+    // console.log ("all tags here", tags)
 
     //for loop
     //then do a matching with product type id
@@ -105,10 +147,10 @@ app.get('/shippingproducts', async (req, res) => {
         }
       }
       modifiedArrayOfItems.push(modifiedItem);
-      console.log("modified array here 1", modifiedArrayOfItems) 
+      // console.log("modified array here 1", modifiedArrayOfItems) 
 
     }
-    console.log("modified array here 2", modifiedArrayOfItems)
+    // console.log("modified array here 2", modifiedArrayOfItems)
     let jsonString = JSON.stringify(modifiedArrayOfItems);
     res.json(jsonString)
   
@@ -178,7 +220,8 @@ app.get('/shippingproducts/:id', async (req, res) => {
   }
 })
 
-app.get('/shippingproducts', async (req, res) => {
+
+app.get('/shippingproducts', authenticateWithJWT, async (req, res) => {
   try {
       // Fetching all recipes
     const allShippingItems = await db.collection('shipping_db').find({}).toArray();
@@ -250,6 +293,181 @@ app.delete('/shippingproducts/:id', async function(req,res){
   })
 })
 
+app.get('/search/:name', async (req, res) => {
+  try {
+    console.log("Test");
+    const name = req.params.name;
+    console.log("Test");
+    const searchProduct = await db.collection('shipping_db').findOne({name: name});
 
+
+    console.log("search product here", searchProduct)
+
+    const tags = await db.collection('product_Type').findOne({_id: searchProduct.product_type});
+
+    console.log ("tags", tags)
+
+
+    if (searchProduct && tags) {
+
+      let responseOutput = {
+        _id: searchProduct._id,
+        name: searchProduct.name,
+        value: searchProduct.value,
+        product_type: tags.type
+      }
+      res.json(responseOutput);
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching product', error: error.message });
+  }
+})
+
+async function connect(uri, dbname) {
+
+  // `connect` allows us to connect to the mongodb
+  // useUnifiedTopology means we want use the latest
+  // structure for Mongo
+  const client = await MongoClient.connect(uri);
+  let db = client.db(dbname);
+  return db;
+}
+
+// A jwt is sometimes known as an 'access token' because it grants access
+// to your services or protected routes
+function generateAccessToken(id, email) {
+  // the first arugment of `jwt.sign` is the payload that you want to store
+  // the second argument of `jwt.sign` is the token secret
+  // the third arugment is an option object
+  return jwt.sign({
+      'user_id': id,
+      'email': email
+  }, process.env.TOKEN_SECRET, {
+      'expiresIn':'3d'  // w = weeks, d = days, h = hours, m = minutes, s = seconds
+  });
+}
+
+// this is a middleware function that check if a valid JWT has been provided
+// a middleware function has three arugments: req, res, next
+
+// First mistake is he never store his JWToken anywhere, so when go to other route, it is GONEEEEEEEEEEEEEE....
+// This is the wrong way to store JWT.
+// let JWTStore = {};
+
+// function authenticateWithJWT(req, res, next) {
+  
+//   // 2nd mistake is, req.headers.authorization does not magically appear out from fantasy land
+//   // req.headers.authorization = JWTStore["access_token"];
+//   console.log(req.headers);
+//   const authHeader = req.headers.authorization;
+//   console.log("authheader here", authHeader)
+//   if (authHeader) {
+
+//       // 3rd mistake: authHeader.split(" ")[1];
+//       // JWT traditionally comes in the form of:
+//       // BEARER "actual hash of the token is here with a space after bearer"
+//       // .split takes each element separated by a empty space " " and put into an array and returns it
+//       // and thus ["BEARER", "JSONHASHBLAHBLAH"]
+//       // thus token = theArray[1] which is the 2nd position which is actual token
+
+//       const token = authHeader.split(" ")[1];
+//       // first argument: the token that I want to verify
+//       // second argument: the token secret
+//       // third argument: callback function
+//       jwt.verify(token, process.env.TOKEN_SECRET, function(err,payload){
+//           if (err) {
+//               res.status(400);
+//               return res.json({
+//                   'error': err
+//               })
+//           } else {
+//               // the JWT is valid, forward request to the route and store the payload in the request
+//               req.payload = payload;
+//               next();
+//           }
+//       })
+//   } else {
+//       res.status(400);
+//       res.json({
+//           'error':'Login required to access this route'
+//       })
+//   }
+
+
+// }
+app.post('/user', async function(req,res){
+
+  // hashing with bcrypt is an async function
+  // bcyrpt.hash takes two argument:
+  // 1. the plaintext that you want to hash
+  // 2. how secure you want it
+  const hashedPassword = await bcrypt.hash(req.body.password, 12);
+  console.log("hashp", hashedPassword)
+  console.log("email", req.body.email)
+  const result = await db.collection('users').insertOne({
+      'email': req.body.email,
+      'password': hashedPassword
+  })
+  res.json({
+      'result': result
+  })
+})
+
+// Allow user to log in by providing their email and password
+app.post('/login', async function(req,res){
+  // 1. Find the user by email address
+  const user = await db.collection('users')
+                  .findOne({
+                      email: req.body.email
+                  });
+
+  
+  // 2. Check if the password matches
+  if (user && Object.keys(user).length !== 0) {
+      // bcrypt.compare()
+      // - first arugment is the plaintext
+      // - second argument is the hashed version 
+      if (await bcrypt.compare(req.body.password, user.password)) {
+          // valid login - so generate the JWT
+          const token = generateAccessToken(user._id, user.email);
+
+          // JWTStore["access_token"] = token;
+
+          res.json({
+              'token': token
+          })
+      } else {
+          res.status(400);
+          res.json({
+              'error':'Invalid login credentials'
+          })
+      }
+  } else {
+      res.status(400);
+      return res.json({
+          'error':'Invalid login credentials'
+      })
+  }
+
+  // 3. Generate and send back the JWT (aka access token)
+});
+
+// Protected route: client must provide the JWT to access
+app.get('/profile', authenticateWithJWT, async function(req,res){
+ 
+  res.json({
+      'message':'success in accessing protected route',
+      'payload': req.payload
+  })
+})
+
+app.get('/payment', authenticateWithJWT, async function(req,res){
+  res.json({
+      'message':"accessing protected payment route"
+  })
+})
+  
 
 
